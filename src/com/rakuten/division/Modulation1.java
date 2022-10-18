ef2c5907c160dc4bc9d49a80f7e28bfc40772273
rupal <xyz@gmail.com>
2022-10-19 01:50:25 +0530

    First Version Modulation

---
+++ src/com/rakuten/division/Modulation1.java
@@ -0,0 +1,12 @@
+package com.rakuten.division;
+
+public class Modulation1 {
+public static void main(String[] args) {
+	double num1 = 45;
+	double num2 = 5;
+	double result = num1/num2;
+	System.out.println("num1 =" + num1);
+	System.out.println("num2 =" + num2);
+	System.out.println("result =" + result);
+}
+}
---
+++ src/com/rakuten/division/Modulation2.java
@@ -0,0 +1,12 @@
+package com.rakuten.division;
+
+public class Modulation2 {
+public static void main(String[] args) {
+	double num1 = 45;
+	double num2 = 25;
+	double result = num1%num2;
+	System.out.println("num1 =" + num1);
+	System.out.println("num2 =" + num2);
+	System.out.println("result =" + result);
+}
+}
